363d84c08c4129eae47ed1f3051d6edb15d0eea1
VixToryH bc2025-3 <Viktoriia.Hvozdevych@lnu.edu.ua>
2026-05-19 20:37:30 +0300

    Initialize Vite React project, install react-router-dom, and add trains mock data

---
+++ src/data/trains.js
@@ -0,0 +1,53 @@
+export const trainsData = [
+    {
+      id: "705K",
+      number: "705К",
+      from: "Київ-Пасажирський",
+      to: "Львів",
+      departureDate: "2026-05-20",
+      departureTime: "06:00",
+      duration: "6г 20хв",
+      wagons: [
+        { id: 1, type: "Інтерсіті (1 клас)", rows: 10 },
+        { id: 2, type: "Інтерсіті (2 клас)", rows: 12 }
+      ]
+    },
+    {
+      id: "091К",
+      number: "091К",
+      from: "Київ-Пасажирський",
+      to: "Львів",
+      departureDate: "2026-05-20",
+      departureTime: "22:37",
+      duration: "7г 43хв",
+      wagons: [
+        { id: 1, type: "Купе", rows: 9 },
+        { id: 2, type: "Плацкарт", rows: 9 }
+      ]
+    },
+    {
+      id: "012Л",
+      number: "012Л",
+      from: "Львів",
+      to: "Одеса-Головна",
+      departureDate: "2026-05-21",
+      departureTime: "21:40",
+      duration: "10г 15хв",
+      wagons: [
+        { id: 1, type: "Купе", rows: 9 },
+        { id: 2, type: "Люкс (СВ)", rows: 6 }
+      ]
+    },
+    {
+      id: "043Л",
+      number: "043Л",
+      from: "Івано-Франківськ",
+      to: "Київ-Пасажирський",
+      departureDate: "2026-05-21",
+      departureTime: "20:04",
+      duration: "8г 46хв",
+      wagons: [
+        { id: 1, type: "Плацкарт", rows: 12 }
+      ]
+    }
+  ];
