e01a5ce93ef4fce78e6eecf706a9be9e76bedda3
VixToryH bc2025-3 <Viktoriia.Hvozdevych@lnu.edu.ua>
2026-05-23 15:30:28 +0300

    connect Booking page to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-
-const BookingPlaceholder = () => (
-  <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
-    <h2>Сторінка бронювання місць</h2>
-    <a href="/" style={{ color: '#0056b3', fontWeight: 'bold' }}>Назад на головну</a>
-  </div>
-);
+import Booking from './pages/Booking';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/booking/:trainId" element={<BookingPlaceholder />} />
+        <Route path="/booking/:trainId" element={<Booking />} />
       </Routes>
     </Router>
   );
